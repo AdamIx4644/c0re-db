43dc0ebd9c1598a3519b13ef1b1dd582e5d80d3c
AdamIx <110720675+AdamIx4644@users.noreply.github.com>
2023-01-12 21:08:09 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from flask import Flask, request, redirect, render_template, url_for
+
+
+
+app = Flask(__name__, static_folder='.')
+
+
+@app.route('/')
+def button():
+    # Generate the OAuth2 URL to redirect the user to the Discord login page
+    return render_template('index.html', members="2016", servers="23")
+
+
+
+
+if __name__ == '__main__':
+    app.run()
