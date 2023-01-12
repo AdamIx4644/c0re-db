b6d14efb38f76398935c9d022a5a3dcf9923626e
AdamIx <110720675+AdamIx4644@users.noreply.github.com>
2023-01-12 22:05:56 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__, static_folder='.')
 @app.route('/')
 def button():
     # Generate the OAuth2 URL to redirect the user to the Discord login page
-    return render_template('index.html', members="2016", servers="23")
+    return "hello world"
 
 
 
